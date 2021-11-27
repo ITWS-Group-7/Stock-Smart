d352f0d2314f9c2e880c65b0a2332bcdfc613dc0
filawt <filawt@rpi.edu>
2021-11-27 10:52:38 -0600

    Update kitchen.html and stocksmart.php
    Retrieving data from database in stocksmart.php for insert_grocery button and dairy food group on kitchen.html, and trying to see how to dynamically load content into table on kitchen.html.

--- stocksmart.php
+++ stocksmart.php
@@ -9,9 +9,24 @@
     if(isset($_POST['insert_grocery'])) {
         $sql_insert_grocery = "INSERT INTO grocery (food_name, food_group, username, bought);
         VALUES ($food, '$foodgroup', $bought, 'TBA');";
-        $stmt = $dbconn->query($sql2);
+        $stmt = $dbconn->query($sql_insert_grocery);
         $prod_arr = $stmt->fetchAll();
         header("Location: grocery.html?insert_grocery=success");
+    }
+
+    if (isset($_GET['action']) && $_GET['action'] == 'dairy') {
+        $sql_print_grocery = "SELECT food_name, food_group, expiration_date, item_opened from food_items WHERE food_group='Dairy';";        
+        $stmt = $dbconn->query($sql_print_grocery);
+        $prod_arr = $stmt->fetchAll();
+        foreach ($prod_arr as $row){
+            echo "<tr><td><a href=" . $row['name']> print $row['name'] . "</a>".
+           "</td><td>" . $row['country'] . "</td></tr>". "</td><td>" . $row['sex']. "</td><td>" .
+           $row['color']. "</td><td>". "</td><td>" .
+            $row['pattern'] . $row['coat']. "</td><td>" . $row['breeders'];
+            //are we reading from json file or database and are we dynamically loading in table from javascript or php file
+
+           }
+        header("Location: grocery.html?print_dairy=success");
     }
 
 ?>
