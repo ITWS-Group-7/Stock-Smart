5af438e2de81d9bccc6dd9fb2044f0002609b1ac
filawt <filawt@rpi.edu>
2021-11-30 11:45:23 -0600

    Update stocksmart.php
    Went off of code in kitchen.js where table is dynamically loaded to have dairy group button work

--- stocksmart.php
+++ stocksmart.php
@@ -18,14 +18,27 @@
         $sql_print_grocery = "SELECT food_name, food_group, expiration_date, item_opened from food_items WHERE food_group='Dairy';";        
         $stmt = $dbconn->query($sql_print_grocery);
         $prod_arr = $stmt->fetchAll();
+        $today = date("Y-m-d H:i:s");
+      
         foreach ($prod_arr as $row){
-            echo "<tr><td><a href=" . $row['name']> print $row['name'] . "</a>".
-           "</td><td>" . $row['country'] . "</td></tr>". "</td><td>" . $row['sex']. "</td><td>" .
-           $row['color']. "</td><td>". "</td><td>" .
-            $row['pattern'] . $row['coat']. "</td><td>" . $row['breeders'];
-            //are we reading from json file or database and are we dynamically loading in table from javascript or php file
-
+            $date = $row['expiration date'];
+           $print= "<tr id=\"hvr-grow-shadow\"> .
+           '<td>' . $row['food_name'] . '</td>' .
+           '<td>' . $row['purchase_date'] . '</td>' .
+           '<td>'. $row['expiration_date'] . '</td>' .
+           '<td>'$row['item_opened'] . '</td>";
+           if ($row['expiration date'] - $today) >= 5){
+            $print . "'<td>'. '<button type=\"button\" class=\"btn btn-danger\"  data-toggle=\"modal\" data-target=\"#warning_modal\">VIEW MORE</button>' + '</td>'";
            }
+           else{
+           $print . "'<td>'+ '<button type=\"button\" class=\"btn btn-success\" data-toggle = \"modal\" data-target = \"#success_modal\">VIEW MORE</button>' + '</td>'";
+         }
+         $print . "'</tr>'";
+        }
+        echo $print;
+
+
+
         header("Location: grocery.html?print_dairy=success");
     }
 
